8727fa7de8f24e15a8a71f6b3737bc9c6462f68c
Azmal Shaikh <azmalshaikh907@gmail.com>
2020-12-27 00:27:34 +0530

    Absolute List Sorting
    https://practice.geeksforgeeks.org/problems/absolute-list-sorting/1

---
+++ linkedList/AbsoluteListSorting.java
@@ -0,0 +1,23 @@
+Node sortedList(Node head)
+	{
+	    
+	    if(head == null || head.next == null) return head;
+	    
+	    Node tail = head.next;
+	    Node cur = head;
+	    
+	    while(tail != null) {
+	        if(tail.data < 0) {
+    	        cur.next = tail.next;
+    	        tail.next = head;
+    	        
+    	        head = tail;
+    	        tail = cur.next;
+	        } else {
+    	        cur = tail;
+    	        tail = tail.next;
+    	    }
+	    }
+	    
+	    return head;
+	}
