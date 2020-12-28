f3da79755e54c4980108732798a083df76df4b2c
Azmal Shaikh <azmalshaikh907@gmail.com>
2020-12-28 17:16:32 +0530

    Insert in a Sorted Lis
    https://practice.geeksforgeeks.org/problems/insert-in-a-sorted-list/1/

---
+++ linkedList/Insertll.java
@@ -0,0 +1,18 @@
+Node sortedInsert(Node head, int key) {
+        Node temp = head;
+        Node toAdd = new Node(key);
+        
+        if(head == null) {
+            head = toAdd;
+        } else if(head.data > key) {
+            toAdd.next = head;
+            head = toAdd;
+        } else {
+            while(temp.next != null && temp.next.data < key) {
+                temp = temp.next;
+            }
+            toAdd.next = temp.next;
+            temp.next = toAdd;
+        }
+        return head;
+    }
