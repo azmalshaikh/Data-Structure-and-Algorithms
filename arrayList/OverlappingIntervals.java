fab833c9be40605a692999d555a5d0f000f7ca4d
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-01-04 22:01:18 +0530

    Merge Overlapping Intervals
    https://practice.geeksforgeeks.org/problems/overlapping-intervals4919/1#

---
+++ arrayList/OverlappingIntervals.java
@@ -0,0 +1,25 @@
+public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        
+    Collections.sort(intervals,new Comparator<Interval>(){
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start-b.start;
+        }
+    });
+    ArrayList<Interval>res=new ArrayList<Interval>();
+    int start=intervals.get(0).start;
+    int end=intervals.get(0).end;
+    for(int i=1;i<intervals.size();i++) {
+        if(intervals.get(i).start<=end) {
+            end=Math.max(intervals.get(i).end,end);
+        }
+        else {
+            res.add(new Interval(start,end));
+            start=intervals.get(i).start;
+            end=intervals.get(i).end;
+        }
+    }
+    res.add(new Interval(start,end));
+
+    return res;
+}
