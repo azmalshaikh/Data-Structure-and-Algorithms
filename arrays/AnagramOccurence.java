8182060295f58b187e9e9c39787d0f39289b6993
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-01-09 16:14:41 +0530

    Count Occurences of Anagrams
    https://practice.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1#

--- arrays/AnagramOccurence.java
+++ arrays/AnagramOccurence.java
@@ -2,33 +2,27 @@ int search(String pat, String txt) {
     int n = txt.length();
     int k = pat.length();
 
-    int arr[] = new int [256];
+    int arr1[] = new int [256];
+    int arr2[] = new int [256];
 
     for(int i=0; i<k; i++) {
-        arr[pat.charAt(i)]++;
+        arr2[pat.charAt(i)]++;
     }
 
     for(int i=0; i<k; i++) {
-        arr[txt.charAt(i)]--;
+        arr1[txt.charAt(i)]++;
     }
 
     int res = 0;
-    if(zero(arr)) res++;
+    if(Arrays.equals(arr1, arr2)) res++;
 
     for(int i=k; i<n; i++) {
-        arr[txt.charAt(i)]--;
+        arr1[txt.charAt(i)]++;
 
-        arr[txt.charAt(i-k)]++;
+        arr1[txt.charAt(i-k)]--;
 
-        if(zero(arr)) res++;
+        if(Arrays.equals(arr1, arr2)) res++;
     }
 
     return res;
-}
-
-boolean zero(int arr[]) {
-    for(int i=0; i<256; i++) {
-        if(arr[i] != 0) return false;
-    }
-    return true;
 }
