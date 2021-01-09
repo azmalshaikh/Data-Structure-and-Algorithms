eaffd7823795341c363e0ac08b1278f4401a643e
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-01-09 20:46:48 +0530

    Count distinct elements in every window
    https://practice.geeksforgeeks.org/problems/count-distinct-elements-in-every-window/1

---
+++ map/DistinctElements.java
@@ -0,0 +1,24 @@
+ArrayList<Integer> countDistinct(int arr[], int n, int k) {
+    ArrayList<Integer> ans = new ArrayList<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for(int i=0; i<k; i++) {
+        map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+    }
+
+    ans.add(map.size());
+
+    for(int i=k; i<n; i++) {
+        if(map.get(arr[i-k]) == 1) {
+            map.remove(arr[i-k]);
+        } else {
+            map.put(arr[i-k], map.get(arr[i-k])-1);
+        }
+
+        map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+
+        ans.add(map.size());
+    }
+
+    return ans;
+}
