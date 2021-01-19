2b084a4df3a155e2c090eef30c55193bbddbec58
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-01-19 23:48:42 +0530

    Postorder Tree Traversal | Iterative

---
+++ binarytree/Postorder.java
@@ -0,0 +1,18 @@
+void postorder(Node root) {
+    Stack<Integer> s = new Stack<();
+    s.push(root);
+
+    Stack<Integer> ans = new Stack<>();
+    while(!s.isEmpty()) {
+        Node cur = s.pop();
+
+        ans.push(cur.data);
+
+        if(cur.left != null) s.push(cur.left);
+        if(cur.right != null) s.push(cur.right);
+    }
+
+    while(!ans.isEmpty()) {
+        System.out.print(ans.pop() + " ");
+    }
+}
