2b3b917ef69e31499f2261809ae32111585ea218
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-01-19 23:53:17 +0530

    Preorder Tree Traversal | Iterative

---
+++ binarytree/Preorder.java
@@ -0,0 +1,14 @@
+void preorder(Node root) {
+    Stack<Node> s = new Stack<>();
+    s.push(root);
+
+    while(!s.isEmpty()) {
+        Node cur = s.pop();
+
+        System.out.print(cur.data + " ");
+
+        if(cur.right != null) s.push(cur.right);
+
+        if(cur.left != null) s.push(cur.left);
+    }
+}
