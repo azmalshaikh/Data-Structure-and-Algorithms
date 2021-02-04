a7edf160cd342ab3ea877bf840d4acf346c395c8
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-02-05 00:36:55 +0530

    Josephus Problem
    https://practice.geeksforgeeks.org/problems/josephus-problem/1

--- recursion/Josephus.java
+++ recursion/Josephus.java
@@ -1,5 +1,5 @@
 public int josephus(int n, int k) {
-    if(n == 1) return 1;
+    if(n == 1) return 0;
 
-    return (josephus(n - 1, k) + k-1) % n + 1 ;
+    return (josephus(n - 1, k) + k) % n ;
 }
