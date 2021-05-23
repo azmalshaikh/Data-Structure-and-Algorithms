a346dd8928f09d479635f6b4487d4aa3bde171a5
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-05-23 18:32:25 +0530

    N Queen Problem
    https://practice.geeksforgeeks.org/problems/n-queen-problem0315/1

--- backtracking/nQueen.java
+++ backtracking/nQueen.java
@@ -1,66 +1,68 @@
-public static void main (String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int t = sc.nextInt();
-    while(t-- > 0) {
-        int n = sc.nextInt();
-        int arr[][] = new int [n][n];
-        boolean ans = nQueen(arr, 0, n);
-        if(!ans) System.out.print(-1);
-        System.out.println();
-    }
+static ArrayList<ArrayList<Integer>> nQueen(int n) {
+    int board[][] = new int [n][n];
 
-}
+    ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    ArrayList<Integer> list  = new ArrayList<>();
 
-static void print(int arr[][], int n) {
-    System.out.print("["); 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(arr[i][j] == 1) System.out.print(j+1 + " ");
-        }
-    }
-    System.out.print("] ");
-} 
+    nQueen(board, 0, n, ans, list);
 
-static boolean nQueen(int arr[][], int x, int n) {
-    if(x == n) {
-        print(arr, n);
-        return true;
-    }
-    boolean ans = false; 
-    for(int col=0; col<n; col++) {
-        if(isSafe(arr, x, col, n)) {
-            arr[x][col] = 1;
 
-            if(nQueen(arr, x+1, n)) {
-                ans = true;
-            }
-
-            arr[x][col] = 0;
-        }
-    }
     return ans;
 }
 
-static boolean isSafe(int arr[][], int x, int y, int n) {
-    for(int i=0; i<x; i++) {
-        if(arr[i][y] == 1) return false;
+static boolean isSafe(int board[][], int x, int y, int n) {
+    for(int i=0; i<n; i++) {
+        if(board[i][y] == 1) return false;
     }
 
-    int row = x;
-    int cols = y;
-    while(row >= 0 && cols >= 0) {
-        if(arr[row][cols] == 1) return false;
+    int row=x;
+    int col=y;
+
+    while(row >= 0 && col >= 0) {
+        if(board[row][col] == 1) return false;
+
         row--;
-        cols--;
+        col--;
     }
 
     row = x;
-    cols = y;
-    while(row >= 0 && cols < n) {
-        if(arr[row][cols] == 1) return false;
+    col = y;
+
+    while(row >=0 && col < n) {
+        if(board[row][col] == 1) return false;
+
         row--;
-        cols++;
+        col++;
     }
 
     return true;
+}
+
+static boolean nQueen(int board[][], int x, int n, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> list) {
+    if(x == n) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j] == 1) {
+                    list.add(j+1);
+                }
+            }
+        }
+        ans.add(new ArrayList<>(list));
+        list.clear();
+        return false;
+    }
+
+    for(int col=0; col<n; col++) {
+        if(isSafe(board, x, col, n)) {
+            board[x][col] = 1;
+
+            if(nQueen(board, x+1, n, ans, list)) {
+                return true;
+            }
+
+            board[x][col] = 0;
+        }
+    }
+
+    return false;
 }
