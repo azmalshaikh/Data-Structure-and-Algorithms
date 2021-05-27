f07a3cfbdf9c9e4c6e8fc3c4b6368f841939e0cb
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-05-27 18:03:09 +0530

    Selection Sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -11,7 +11,7 @@ public class Main {
         for(int i=0; i<n-1; i++) {
             int min = i;
 
-            for(int j=i; j<n; j++) {
+            for(int j=i+1; j<n; j++) {
                 if(arr[min] > arr[j]) {
                     min = j;
                 }
