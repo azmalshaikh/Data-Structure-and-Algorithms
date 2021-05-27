300988885be4eb2131279bcb5007578a106225cf
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-05-27 18:02:08 +0530

    Selection Sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String args[]) {
+        int arr[] = {2, 1, 8, -3, 6, 4, 12};
+        selectionSort(arr, 7);
+        for(int e : arr) {
+            System.out.print(e + " ");
+        }
+    }
+
+    static void selectionSort(int arr[], int n) {
+        for(int i=0; i<n-1; i++) {
+            int min = i;
+
+            for(int j=i; j<n; j++) {
+                if(arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+
+            if(min != i) {
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
