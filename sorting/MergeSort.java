61200e038477eff43a46c5302c09f4ecfa8ba5b4
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-06-03 23:54:45 +0530

    Merge Sort
    https://practice.geeksforgeeks.org/problems/merge-sort/1#

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -1,49 +1,46 @@
-void mergeSort(int arr[], int start, int end) {
-    if(start < end) {
-        int mid = (start+end)/2;
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid+1, end);
-        
-        merge(arr,l,mid,r);
+public class Main {
+    public static void main(String args[]) {
+        int arr[] = {9, 4, 7, 6, 3, 1, -5};
+        mergeSort(arr, 0, arr.length-1);
+        for(int i=0; i<arr.length; i++) System.out.print(arr[i] + " ");
+    }
+
+    static void mergeSort(int arr[], int l, int r) {
+        if(l<r) {
+            int mid = (l+r)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+
+    static void merge(int arr[], int l, int mid, int r) {
+        int temp[] = new int [arr.length];
+        int k = l, i=l, j=mid+1;
+
+        while(i<=mid && j<=r) {
+            if(arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        if(i>mid) {
+            while(j<=r) {
+                temp[k++] = arr[j++];
+            }
+        } else {
+            while(i<=mid) {
+                temp[k++] = arr[i++];
+            }
+        }
+
+        for(k=l; k<=r; k++) {
+            arr[k] = temp[k];
+        }
     }
 }
-
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    int a[] = new int[n1];
-    int b[] = new int[n2];
-
-    for (int i = 0; i < n1; i++)
-        a[i] = arr[l + i];
-
-    for (int j = 0; j < n2; j++)
-        b[j] = arr[m + 1 + j];
-
-    int i=0, j=0, k=l;
-
-    while (i < n1 && j < n2) {
-        if (a[i] <= b[j]) {
-            arr[k] = a[i];
-            i++;
-        }
-        else {
-            arr[k] = b[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = a[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = b[j];
-        j++;
-        k++;
-    }
- }
