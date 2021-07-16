bc2487782e6831993902cd5341a5ff9aac66f797
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-07-16 23:40:55 +0530

    Find And Nodetorootpath In Binary Tree
    https://www.pepcoding.com/resources/online-java-foundation/binary-tree/find-nodetorootpath-binary-tree-official/ojquestion

--- binarytree/FindAndPath.java
+++ binarytree/FindAndPath.java
@@ -1,46 +1,30 @@
 public static boolean find(Node node, int data){
   if(node == null) return false;
 
-  if(node.data == data) return true;
-
-  if(node.left != null) {
-      if(find(node.left, data)) return true;
+  if(node.data == data) {
+      list.add(node.data);
+      return true;
   }
 
-  if(node.right != null) {
-      if(find(node.right, data)) return true;
+  boolean filc = find(node.left, data);
+  if(filc) {
+      list.add(node.data);
+      return true;
+  }
+
+  boolean firc = find(node.right, data);
+  if(firc) {
+      list.add(node.data);
+      return true;
   }
 
   return false;
 }
 
 public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    return list;
+}
 
-    if(node == null) {
-        return new ArrayList<>();
-    }
-
-  ArrayList<Integer> list = new ArrayList<>();
-
-  if(node.data == data) {
-      list.add(node.data);
-      return list;
-  }
-
-  ArrayList<Integer> l1 = new ArrayList<>();
-  ArrayList<Integer> l2 = new ArrayList<>();
-
-  if(node.left != null) {
-      l1 = nodeToRootPath(node.left, data);
-  } 
-
-  if(node.right != null) {
-      l2 = nodeToRootPath(node.right, data);
-  }
-
-  if(!l1.isEmpty()) list.addAll(l1);
-  if(!l2.isEmpty()) list.addAll(l2);
-  if(!list.isEmpty())list.add(node.data);
-
-  return list;
+public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    return list;
 }
