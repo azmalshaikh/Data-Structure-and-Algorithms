e704641e1bbd431c08e8159788d4bc752b3a0e95
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-07-30 23:44:20 +0530

    Number Of Enclaves
    https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/graphs/number-of-enclaves-official/ojquestion#!

---
+++ graphs/Enclave.java
@@ -0,0 +1,45 @@
+public static void connectedComp(int arr[][], boolean vis[][], int cr, int cc) {
+  if(cr < 0 || cc < 0 || cr >= arr.length || cc >= arr[0].length || arr[cr][cc] == 0 || vis[cr][cc]) return;
+
+  vis[cr][cc] = true;
+  arr[cr][cc] = 0;
+  connectedComp(arr, vis, cr-1, cc);
+  connectedComp(arr, vis, cr+1, cc);
+  connectedComp(arr, vis, cr, cc+1);
+  connectedComp(arr, vis, cr, cc-1);
+}
+
+public static int numEnclaves(int[][] arr) {
+  int m = arr.length;
+  int n = arr[0].length;
+  boolean vis[][] = new boolean [m][n];
+  for(int j=0; j<n; j++) {
+      if(arr[0][j] == 1) {
+          connectedComp(arr, vis, 0, j);
+      }    
+
+      if(arr[m-1][j] == 1) {
+          connectedComp(arr, vis, m-1, j);
+      }
+  }
+
+  for(int i=0; i<m; i++) {
+      if(arr[i][0] == 1) {
+          connectedComp(arr, vis, i, 0);
+      }    
+
+      if(arr[i][n-1] == 1) {
+          connectedComp(arr, vis, i, n-1);
+      }
+  }
+
+  int ans = 0;
+
+  for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++) {
+          if(arr[i][j] == 1) ans++;
+      }
+  }
+
+  return ans;
+}
