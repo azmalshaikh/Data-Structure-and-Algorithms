ff957d76ee1e0944b14a71c736c005edb0382497
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-07-31 21:24:38 +0530

    Bridge edge in graph
    https://practice.geeksforgeeks.org/problems/bridge-edge-in-graph/1#

---
+++ graphs/Bridge.java
@@ -0,0 +1,49 @@
+class Solution {
+    //Function to find if the given edge is a bridge in graph.
+    static int isBridge(int V, ArrayList<ArrayList<Integer>> adj,int c,int d) {
+        timer = 0;
+        in = new int [V];
+        low = new int [V];
+        ans = false;
+        boolean vis[] = new boolean [V];
+        for(int i=0; i<V; i++) {
+            if(!vis[i]) dfs(adj, i, -1, vis, c, d);
+        }
+        
+        if(ans) return 1;
+        
+        return 0;
+        
+    }
+    static int timer;
+    static int in[];
+    static int low[];
+    static boolean ans;
+    
+    static void dfs(ArrayList<ArrayList<Integer>> adj, int src, int parent, boolean vis[], int c, int d) {
+        vis[src] = true;
+        in[src] = low[src] = timer;
+        timer++;
+        
+        for(int nbr: adj.get(src)) {
+            if(nbr == parent) {
+                continue;
+            }
+            
+            if(vis[nbr]) { // back edge
+                low[src] = Math.min(low[src], in[nbr]);
+            } else { // Forward Edge
+                dfs(adj, nbr, src, vis, c, d);
+                
+                if(low[nbr] > low[src]) {
+                    if( (c==src && d==nbr) || (d==src && c==nbr) ) {
+                        ans = true;
+                        return;
+                    }
+                }
+                
+                low[src] = Math.min(low[src], low[nbr]);
+            }
+        }
+    }
+}
