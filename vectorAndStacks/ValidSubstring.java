708d2058541bf49a647ea1cf65568c16863f9ced
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-08-27 22:37:46 +0530

    Valid Substring
    https://practice.geeksforgeeks.org/problems/valid-substring0624/1

--- vectorAndStacks/ValidSubstring.java
+++ vectorAndStacks/ValidSubstring.java
@@ -20,3 +20,45 @@ static int findMaxLen(String s) {
 
     return ans;
 }
+
+
+M - II
+class Solution {
+    
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlen = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if(ch == '(') {
+                left++;
+            } else if(ch == ')') {
+                right++;
+            }
+            
+            if(left == right) maxlen = Math.max(maxlen, 2*right);
+            else if(right > left) {
+                left = right = 0;
+            } 
+        }
+        
+        left = right = 0;
+        for(int i=s.length()-1; i>=0; i--) {
+            char ch = s.charAt(i);
+            
+            if(ch == '(') {
+                left++;
+            } else if(ch == ')') {
+                right++;
+            }
+            
+            if(left == right) maxlen = Math.max(maxlen, 2*right);
+            else if(left > right) {
+                left = right = 0;
+            } 
+        }
+        
+        return maxlen;
+    }
+}
