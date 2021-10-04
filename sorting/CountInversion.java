639a8cf5e3131666bdde5e4ee170608a01b62540
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-10-04 13:13:02 +0530

    Count Inversions
    https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1#

--- sorting/CountInversion.java
+++ sorting/CountInversion.java
@@ -1,3 +1,66 @@
+// M - I
+class Solution {
+    
+    static long count; 
+
+    static long inversionCount(long arr[], long N){
+        count = 0L;
+        
+        long temp[] = new long[(int)N];
+        mergeSort(arr, 0, N-1, temp);
+        
+        return count;
+    }
+    
+    public static void mergeTwoArrays(long arr[], long low, long mid, long high, long merged[]) {
+        
+        // 1st array low - mid
+        // 2nd array mid+1 - high
+        
+        long i=low, j=mid+1, vidx = low;
+        
+        while(i<=mid && j<=high) {
+            if(arr[(int)i] <= arr[(int)j]) {
+                merged[(int)vidx++] = arr[(int)i++];
+            } else {
+                count += mid + 1 - i;
+                
+                merged[(int)vidx++] = arr[(int)j++];
+            }
+        }
+        
+        while(i<=mid) {
+            merged[(int)vidx++] = arr[(int)i++];
+        }
+        
+        while(j<=high) {
+            merged[(int)vidx++] = arr[(int)j++];
+        }
+        
+        for(long val=low; val<=high; val++) {
+            arr[(int)val] = merged[(int)val];
+        }
+    }
+    
+    public static void mergeSort(long arr[], long low, long high, long temp[]) {
+        
+        if(low < high) {
+            long mid = (low+high) >> 1;
+        
+            mergeSort(arr, low, mid, temp);
+            mergeSort(arr, mid+1, high, temp);
+        
+            mergeTwoArrays(arr, low, mid, high, temp);
+            // 1st array low - mid
+            // 2nd array mid+1 - high
+        }
+    }
+    
+    
+}
+
+
+// M - II
 private static int mergeCount(int[] arr, int l, int m, int r) { 
 
     int[] left = Arrays.copyOfRange(arr, l, m + 1); 
@@ -33,4 +96,71 @@ private static int mergeSort(int[] arr, int l, int r) {
     } 
 
     return count; 
+}
+
+
+// M-II
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static int count;
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        count = 0;
+        mergeSort(arr, 0, n-1);
+        System.out.println(count);
+    }
+    
+    public static int[] mergeTwoArrays(int left[], int right[]) {
+        int merged[] = new int[left.length + right.length];
+        
+        int i=0, j=0, vidx = 0;
+        
+        while(i<left.length && j<right.length) {
+            if(left[i] <= right[j]) {
+                merged[vidx++] = left[i++];
+            } else {
+                count += left.length - i;
+                
+                merged[vidx++] = right[j++];
+            }
+        }
+        
+        while(i<left.length) {
+            merged[vidx++] = left[i++];
+        }
+        
+        while(j<right.length) {
+            merged[vidx++] = right[j++];
+        }
+        
+        return merged;
+    }
+    
+    public static int[] mergeSort(int arr[], int low, int high) {
+        if(low == high) {
+            int temp[] = new int[1];
+            temp[0] = arr[low];
+            return temp;
+        }
+        
+        int mid = (low+high)/2;
+        
+        int left[] = mergeSort(arr, low, mid);
+        int right[] = mergeSort(arr, mid+1, high);
+        
+        int merged[] = mergeTwoArrays(left, right);
+        
+        return merged;
+    }
 }
