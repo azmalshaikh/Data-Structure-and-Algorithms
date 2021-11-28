3130877af309b88c37596666cb5fdb15489689bd
Azmal Shaikh <azmalshaikh907@gmail.com>
2021-11-28 19:12:19 +0530

    Alien Dictionary
    https://practice.geeksforgeeks.org/problems/alien-dictionary/1#

--- graphs/AlienDictionary2.java
+++ graphs/AlienDictionary2.java
@@ -1,3 +1,4 @@
+// M-I
 public String findOrder(String [] dict, int N, int K) {
     ArrayList<ArrayList<Character>> adj = new ArrayList<>();
 
@@ -40,4 +41,61 @@ public String findOrder(String [] dict, int N, int K) {
 
     sb.append('\n');
     return sb.toString();
+}
+
+
+// M-II
+class Solution {
+    public String findOrder(String [] words, int N, int K) {
+        HashMap<Character, HashSet<Character>> graph = new HashMap<>();
+        HashMap<Character, Integer> indegree = new HashMap<>();
+        
+        for(String st: words) {
+            for(char ch: st.toCharArray()) {
+                indegree.put(ch, 0);
+            }
+        }
+        
+        for(int i=0; i<words.length-1; i++) {
+            for(int j=0; j<Math.min(words[i].length(), words[i+1].length()); j++) {
+                if(words[i].charAt(j) != words[i+1].charAt(j)) {
+                    HashSet<Character> temp = graph.getOrDefault(words[i].charAt(j), new HashSet<>());
+                    temp.add(words[i+1].charAt(j));
+                    indegree.put(words[i+1].charAt(j), indegree.get(words[i+1].charAt(j))+1);
+                    graph.put(words[i].charAt(j), temp);
+                    break;
+                }
+            }
+        }
+        
+        Queue<Character> q = new LinkedList<>();
+        
+        for(Character ch: indegree.keySet()) {
+            if(indegree.get(ch) == 0) {
+                q.add(ch);
+            }
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        
+        while(!q.isEmpty()) {
+            Character ch = q.remove();
+            
+            ans.append(ch);
+            
+            if(graph.containsKey(ch)) {
+                for(Character temp: graph.get(ch)) {
+                    indegree.put(temp, indegree.get(temp)-1);
+                    
+                    if(indegree.get(temp) == 0) {
+                        q.add(temp);
+                    }
+                }
+            }
+        }
+        
+        if(ans.length() != indegree.size()) return "";
+        
+        return ans.toString();
+    }
 }
